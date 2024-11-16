45fa5ce18f0c99a45a7e292eda6ac0b671a9fc50
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-15 21:59:30 -0600

    transformer to the Dataset

--- server/model/data_frame_preparer.py
+++ server/model/data_frame_preparer.py
@@ -12,3 +12,23 @@ num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy = "median")),
     ('rbst_scaler', RobustScaler())
 ])
+
+# Transformador que prepara todo el DataSet 
+class DataFramePreparer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self._full_pipeline = None
+        self._columns = None
+    def fit(self, x, y = None):
+        num_attribs = list(x.select_dtypes(exclude = ['object']))
+        cat_attribs = list(x.select_dtypes(include = ['object']))
+        self._full_pipeline = ColumnTransformer([
+            ("num", num_pipeline, num_attribs),
+            ("cat", CustomOneHotEncoder(), cat_attribs),
+        ])
+        self._full_pipeline.fit(x)
+        self._columns = pd.get_dummies(x).columns
+        return self
+    def transform(self, x, y = None):
+        x_copy = x.copy()
+        x_prep = self._full_pipeline.transform(x_copy)
+        return pd.DataFrame(x_prep, columns = self._columns, index = x_copy.index)
