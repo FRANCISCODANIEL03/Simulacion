c9f35d16ad63cebe325c77ed3cbfb2db0f1018d9
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-16 22:15:52 -0600

    add method that predict metrics

--- server/model/train_algorith.py
+++ server/model/train_algorith.py
@@ -12,3 +12,14 @@ x_train_prep, x_val_prep = dsPreparer()
 clf = LogisticRegression(max_iter = 5000)  
 clf.fit(x_train_prep, y_train)
 
+
+def predict():
+    x_test_prep = DSpreparer(x_test)
+    y_pred = clf.predict(x_test_prep)
+
+    y_predR = clf.predict(x_val_prep)
+
+    f1score = f1_score(y_test, y_pred, pos_label='anomaly')
+    recall = recall_score(y_val, y_predR, pos_label = 'anomaly')
+    precision =  precision_score(y_val, y_predR, pos_label = 'anomaly')
+    return f1score, precision, recall
