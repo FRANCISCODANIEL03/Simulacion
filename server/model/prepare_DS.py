082d439ffd573e166e6b5aa9942b2b7dcace8b24
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-16 22:11:25 -0600

    add method that prepare the DataSet

--- server/model/prepare_DS.py
+++ server/model/prepare_DS.py
@@ -9,3 +9,14 @@ x_test, y_test = dsTest()
 
 data_preparer = DataFramePreparer()
 
+def dsPreparer():
+    # Hacer el fit con el DataSet General para que adquiera todos los valores posibles 
+    data_preparer.fit(x_df)
+
+    # Transformar el DataSet de entrenamiento
+    x_train_prep = data_preparer.transform(x_train)
+
+    # Transformar el DataSet de validacion 
+    x_val_prep = data_preparer.transform(x_val)
+    return x_train_prep, x_val_prep
+
