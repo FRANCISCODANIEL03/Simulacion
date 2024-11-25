2647a4676be54aae68d2e81127b4477fa0a73002
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-24 22:21:13 -0600

    train algorithm

--- server/model/metrics.py
+++ server/model/metrics.py
@@ -5,3 +5,12 @@ from create_prep_DS import create_prep_dataset
 
 # Leer 15,000 correos elctrónicos 
 x, y = create_prep_dataset("/home/pako0311/Escritorio/Simulacion/datasets/datasets/trec07p/full/index", 70000)
+
+# Utilizamos 10000 correos para entrenar el algoritmo y 2000 para realizar pruebas 
+x_train, y_train = x[:60000], y[:60000]
+x_test, y_test = x[60000:], y[60000:]
+
+    
+
+
+
