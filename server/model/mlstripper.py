e3d819a6f274800c18089eaa8bb634493b211315
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-25 22:23:36 -0600

    add new class to preprocessing emails

---
+++ server/model/mlstripper.py
@@ -0,0 +1,15 @@
+# En esta clase facilita el preprocesamiento de correos electrónicos que poseen código HTML
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
+    def handle_data (self,d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+    
