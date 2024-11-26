386dd4d90f30e9d8249c56f7ea7577f9ccb11760
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-25 22:27:35 -0600

    add function to extract the email content

--- server/model/parser.py
+++ server/model/parser.py
@@ -14,3 +14,15 @@ class Parser:
         with open(email_path, errors='ignore') as e:
             msg = email.message_from_file(e)
         return None if not msg else self.get_email_content(msg)
+
+    def get_email_content(self, msg):
+        """Extract the email content"""
+        subject = self.tokenize(msg['Subject']) if msg['Subject'] else []
+        body = self.get_email_body(msg.get_payload(),
+                                   msg.get_content_type())
+        content_type = msg.get_content_type()
+        # returning de content of the email
+        return {"subject":subject,
+                "body":body,
+                "content_type":content_type}
+    
