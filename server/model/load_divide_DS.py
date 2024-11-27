98e3274f0fca22bf90e9b5bca34059605e3ef786
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-27 14:04:05 -0600

    remove unusual import

--- server/model/load_divide_DS.py
+++ server/model/load_divide_DS.py
@@ -1,6 +1,5 @@
 from load_kdd_dataset import load_kdd_dataset
 from train_val_test_split import train_val_test_split 
-import pandas as pd 
 
 df = load_kdd_dataset("/home/pako0311/Escritorio/Simulacion/datasets/datasets/NSL-KDD/KDDTrain+.arff")
 
