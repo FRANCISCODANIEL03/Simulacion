cbc0f11ef12df44e74b2bbef1c4eadfffb270c73
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-27 14:05:20 -0600

    add new values

--- server/model/main.py
+++ server/model/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 from train_algorith import predict
+from metrics import predict_acc
 
 app = Flask(__name__)
 CORS(app)
@@ -8,8 +9,13 @@ CORS(app)
 @app.route("/metrics", methods = ["GET"])
 @cross_origin()
 def metrics():
+    accuracy = predict_acc()
     f1score, precision, recall = predict()
     return jsonify([
+        {
+            "name":"Accuracy",
+            "porcent":accuracy
+        },
         {
             "name":"F1 score",
             "porcent":f1score
