895e44e979a6c6311befa7e09f3d1bea21099c25
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-27 14:01:43 -0600

    add function to the boton extraer

--- server/frontend/datos.js
+++ server/frontend/datos.js
@@ -6,3 +6,10 @@ const tbl = document.getElementById("tbl-body")
 const div_main = document.getElementById("div_main")
 
 URL = "http://localhost:5000/metrics2"
+btn_met.addEventListener('click', async (e)=>{
+    e.preventDefault()
+    extraerDat()
+    div_tbl.style.display = 'flex'
+    div_main.classList.add("dvs")
+    div_main.classList.remove("dvs3")
+})
