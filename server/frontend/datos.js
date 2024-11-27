bb22bbe6478f5bc577aed31a6c74002fa0ffb87f
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-27 14:00:53 -0600

    add consts

---
+++ server/frontend/datos.js
@@ -0,0 +1,8 @@
+const btn_met = document.getElementById("btn-met")
+const btn_view = document.getElementById("btn-view")
+const div_imgs = document.getElementById("div_imgs")
+const div_tbl = document.getElementById("div_tbl")
+const tbl = document.getElementById("tbl-body")
+const div_main = document.getElementById("div_main")
+
+URL = "http://localhost:5000/metrics2"
