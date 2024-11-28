ce89e7ba578a9fe34c9b0f9310437762f241dd38
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-28 14:07:27 -0600

    add method to divide train set

--- support_vector_machine/API/divide_dataset.py
+++ support_vector_machine/API/divide_dataset.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("/home/pako0311/Escritorio/Simulacion/datasetsSVM/Phishing.csv")
+
+train_set, val_set, test_set = train_val_test_split(df)
+
+def d_train_set():
+    x_train = train_set.drop('URL_Type_obf_Type', axis=1)
+    y_train = train_set['URL_Type_obf_Type'].copy()
+    return x_train, y_train
+
