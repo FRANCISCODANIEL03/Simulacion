2c68b44f3f2f9e08e485a529cb3599366a073f83
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-28 14:10:21 -0600

    add imports and sets

---
+++ support_vector_machine/API/metrics.py
@@ -0,0 +1,12 @@
+from reduced_ds import red_ds
+from divide_dataset import d_val_set, d_train_set
+from prepare_ds import prepare_dataset
+from sklearn.svm import SVC
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
+
+x_train_reduced, x_val_reduced = red_ds()
+x_val, y_val = d_val_set()
+x_train, y_train = d_train_set()
+x_train_prep, x_val_prep, x_test_prep = prepare_dataset()
