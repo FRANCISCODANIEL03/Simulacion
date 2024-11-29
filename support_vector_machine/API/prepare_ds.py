64ec19ff207e3f0323d9ea2e138e6c4fcea2088c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-29 14:18:47 -0600

    define the variables

--- support_vector_machine/API/prepare_ds.py
+++ support_vector_machine/API/prepare_ds.py
@@ -1,3 +1,7 @@
 from sklearn.impute import SimpleImputer
 from divide_dataset import d_train_set, d_val_set, d_test_set
 import pandas as pd
+
+x_train2, y_train = d_train_set()
+x_val2, y_val = d_val_set()
+x_test2, y_test = d_test_set()
