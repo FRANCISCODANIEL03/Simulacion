79715a84563797fe38f0c6c82dad18b9d423e6cf
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-29 14:16:35 -0600

    define the variables

--- support_vector_machine/API/polynomial_k.py
+++ support_vector_machine/API/polynomial_k.py
@@ -5,3 +5,12 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score
+
+x_train, y_train = d_train_set()
+x_val, y_val = d_val_set()
+x_train_reduced, x_val_reduced = red_ds()
+
+# Para representar el limite de decision de tiene que pasar la variable objetivo a numerica 
+y_train_num = y_train.factorize()[0]
+y_val_num = y_val.factorize()[0]
+
