c20e57cd75678282340e1cd72aea3def49791520
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-29 14:14:05 -0600

    add function to create the second graph

--- support_vector_machine/API/plot_svm_b.py
+++ support_vector_machine/API/plot_svm_b.py
@@ -20,3 +20,20 @@ def plot_svm_decision_boundary(svm_clf, xmin, xmax):
     plt.plot(x0, decision_boundary, 'k-', linewidth=2)
     plt.plot(x0, gutter_up, 'k--', linewidth=2)
     plt.plot(x0, gutter_down, 'k--', linewidth=2)
+
+def plot_svc_decision_boundary(svm_clf, xmin, xmax):
+    w = svm_clf.coef_[0]
+    b = svm_clf.intercept_[0]
+
+    x0 = np.linspace(xmin, xmax, 200)
+    desicion_boundary = -w[0]/w[1] * x0 - b/w[1] 
+
+    margin = 1/w[1]
+    gutter_up = desicion_boundary + margin
+    gutter_down = desicion_boundary - margin
+
+    svs = svm_clf.support_vectors_
+    plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#ffaaaa')
+    plt.plot(x0, desicion_boundary, 'k-', linewidth=2)
+    plt.plot(x0, gutter_up, 'k--', linewidth=2)
+    plt.plot(x0, gutter_down, 'k--', linewidth=2)
