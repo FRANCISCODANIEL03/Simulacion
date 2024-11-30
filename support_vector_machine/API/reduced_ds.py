27405cc4bb252b8fb9a922c8246b1eb02aadbe72
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-30 14:21:51 -0600

    add function to reduced the data set

--- support_vector_machine/API/reduced_ds.py
+++ support_vector_machine/API/reduced_ds.py
@@ -1,3 +1,10 @@
 from prepare_ds import prepare_dataset
 
 x_train_prep, x_val_prep, x_test_prep = prepare_dataset()
+
+def red_ds():
+    # Reducir el DataSet para representarlo graficamente 
+    x_train_reduced = x_train_prep[['domainUrlRatio', 'domainlength'].copy()]
+    x_val_reduced = x_val_prep[['domainUrlRatio','domainlength'].copy()]
+
+    return x_train_reduced, x_val_reduced
