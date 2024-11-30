446c4bcc4b9e1f7c831e849ce1fc32d26a6e536f
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-11-30 14:22:58 -0600

    add function to train the dataset

--- support_vector_machine/API/train_split.py
+++ support_vector_machine/API/train_split.py
@@ -1,2 +1,11 @@
 from sklearn.model_selection import train_test_split
 
+# Construcción de una función que realice le particionado completo
+def train_val_test_split(df, rsate = 42, shuffle = True, stratify = None):
+    strat = df[stratify] if stratify else None
+    train_set, test_set = train_test_split(
+        df, test_size = 0.4, random_state = rsate, shuffle = shuffle, stratify = strat)
+    strat = test_set[stratify] if stratify else None
+    val_set, test_set = train_test_split(
+        test_set, test_size = 0.5, random_state = rsate, shuffle = shuffle, stratify = strat)
+    return (train_set, val_set, test_set)
