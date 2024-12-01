f42cbff59c8b84ef79742b4d8276fdd541f9a609
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-12-01 17:32:37 -0600

    define the variables

--- support_vector_machine/TREE/escalar_ds.py
+++ support_vector_machine/TREE/escalar_ds.py
@@ -1,3 +1,7 @@
 from sklearn.preprocessing import RobustScaler
 from pandas import DataFrame
 from read_csv import divide_DS_train, divide_DS_val, divide_DS_test
+
+X_train, y_train = divide_DS_train()
+X_val, y_val = divide_DS_val()
+X_test, y_test = divide_DS_test()
