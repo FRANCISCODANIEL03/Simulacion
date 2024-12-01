5b4d000bf7e876b21cbc2a234587ad62451f4f29
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-12-01 17:33:12 -0600

    add function to train the train data set

--- support_vector_machine/TREE/escalar_ds.py
+++ support_vector_machine/TREE/escalar_ds.py
@@ -5,3 +5,10 @@ from read_csv import divide_DS_train, divide_DS_val, divide_DS_test
 X_train, y_train = divide_DS_train()
 X_val, y_val = divide_DS_val()
 X_test, y_test = divide_DS_test()
+
+def x_train_scaled():
+    scaler = RobustScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    # Transformación a un DataFrame de Pandas
+    X_train_scaled = DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index)
+    return X_train_scaled
