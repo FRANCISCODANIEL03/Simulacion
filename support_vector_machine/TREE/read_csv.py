c265fb8a242570dfbbdae379cb1bad927e0995ff
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-12-02 17:38:15 -0600

    copy the dataset

--- support_vector_machine/TREE/read_csv.py
+++ support_vector_machine/TREE/read_csv.py
@@ -2,3 +2,8 @@ import pandas as pd
 from funciones_aux import train_val_test_split, remove_labels
 
 df = pd.read_csv('/home/pako0311/Escritorio/Simulacion/datasets/datasets/TotalFeatures-ISCXFlowMeter.csv')
+
+# Copiamos el conjunto de datos y transformamos la variable 
+# de salida a numérica para calcular correlaciones
+X = df.copy()
+X['calss'] = X['calss'].factorize()[0]
