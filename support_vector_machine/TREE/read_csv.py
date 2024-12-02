1ddfe0342b52b0422bf07c9fd3a44a7c402d9f80
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-12-02 17:37:53 -0600

    read the data set

--- support_vector_machine/TREE/read_csv.py
+++ support_vector_machine/TREE/read_csv.py
@@ -1,2 +1,4 @@
 import pandas as pd
 from funciones_aux import train_val_test_split, remove_labels
+
+df = pd.read_csv('/home/pako0311/Escritorio/Simulacion/datasets/datasets/TotalFeatures-ISCXFlowMeter.csv')
