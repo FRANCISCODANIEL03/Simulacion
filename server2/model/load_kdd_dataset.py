96f2fd6a82397331b6932a0baf0949188c102e27
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-01-14 09:41:09 -0600

    fit (fix attributes to list)

--- server2/model/load_kdd_dataset.py
+++ server2/model/load_kdd_dataset.py
@@ -5,5 +5,5 @@ def load_kdd_dataset(data_path):
     """Lectura de DataSet NSL-KDD."""
     with open(data_path, 'r') as train_set:
         dataset = arff.load(train_set)
-    attributes = [attr[0] for attr in dataset["attributes"]]
+    attributes = [attr[0] for attr in list(dataset["attributes"])]
     return pd.DataFrame(dataset["data"], columns = attributes)
