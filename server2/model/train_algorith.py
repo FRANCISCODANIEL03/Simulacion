6046226711a649844c425ab9c4ef770c32043395
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-01-15 11:12:35 -0600

    optimize model

--- server2/model/train_algorith.py
+++ server2/model/train_algorith.py
@@ -3,23 +3,39 @@ from load_divide_DS import dsTrain, dsTest, dsVal
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression
 
-# Entrenar el algoritmo basado en Regresión Logistica 
-x_val, y_val = dsVal()
-x_test, y_test = dsTest()
-x_train, y_train = dsTrain()
-x_train_prep, x_val_prep = dsPreparer()
+# Variables globales inicializadas como None
+clf = None
+x_test, y_test = None, None
+x_val, y_val = None, None
+x_val_prep = None
 
-clf = LogisticRegression(max_iter = 5000)  
-clf.fit(x_train_prep, y_train)
+def initialize_model():
+    global clf, x_test, y_test, x_val, y_val, x_val_prep
+    # Cargar datos
+    x_val, y_val = dsVal()
+    x_test, y_test = dsTest()
+    x_train, y_train = dsTrain()
+    x_train_prep, x_val_prep = dsPreparer()
 
+    # Entrenar modelo
+    clf = LogisticRegression(max_iter=5000)
+    clf.fit(x_train_prep, y_train)
 
 def predict():
+    global clf, x_test, y_test, x_val, y_val, x_val_prep
+
+    # Asegurar que el modelo esté inicializado
+    if clf is None:
+        initialize_model()
+
+    # Preparar los datos de prueba
     x_test_prep = DSpreparer(x_test)
     y_pred = clf.predict(x_test_prep)
 
     y_predR = clf.predict(x_val_prep)
 
-    f1score = f1_score(y_test, y_pred, average ="macro")
-    recall = recall_score(y_val, y_predR, average ="macro")
-    precision =  precision_score(y_val, y_predR, average ="macro")
+    # Calcular métricas
+    f1score = f1_score(y_test, y_pred, average="macro")
+    recall = recall_score(y_val, y_predR, average="macro")
+    precision = precision_score(y_val, y_predR, average="macro")
     return f1score, precision, recall
