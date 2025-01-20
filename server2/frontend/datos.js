c177c758e459b31988685a6e65b49cdd1729748d
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-01-19 20:17:08 -0600

    change route

--- server2/frontend/datos.js
+++ server2/frontend/datos.js
@@ -5,7 +5,7 @@ const div_tbl = document.getElementById("div_tbl")
 const tbl = document.getElementById("tbl-body")
 const div_main = document.getElementById("div_main")
 
-URL = "http://127.0.0.1:5000/metrics"
+URL = "https://model-final.onrender.com/metrics"
 btn_met.addEventListener('click', async (e)=>{
     e.preventDefault()
     extraerDat()
