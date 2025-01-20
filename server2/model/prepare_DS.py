6ebb3c5162919c3decc2cfbb3ba8c780c0cd875a
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-01-19 20:05:42 -0600

    prepare dataset

--- server2/model/prepare_DS.py
+++ server2/model/prepare_DS.py
@@ -1,26 +1,25 @@
 from data_frame_preparer import DataFramePreparer
 from load_divide_DS import datasetGen, dsTrain, dsVal, dsTest
 
-# instanciamos nuestro transformador personalizado
+# Instanciar el transformador personalizado y cargar los datos
+data_preparer = DataFramePreparer()
 x_df, y_df = datasetGen()
 x_train, y_train = dsTrain()
 x_val, y_val = dsVal()
 x_test, y_test = dsTest()
 
-data_preparer = DataFramePreparer()
+# Ajustar el transformador solo una vez
+data_preparer.fit(x_df)
 
+def transform_dataset(dataset):
+    """Transforma un dataset usando el preparador de datos."""
+    return data_preparer.transform(dataset)
+
+# Funciones para preparar datasets específicos
 def dsPreparer():
-    # Hacer el fit con el DataSet General para que adquiera todos los valores posibles 
-    data_preparer.fit(x_df)
-
-    # Transformar el DataSet de entrenamiento
-    x_train_prep = data_preparer.transform(x_train)
-
-    # Transformar el DataSet de validacion 
-    x_val_prep = data_preparer.transform(x_val)
+    x_train_prep = transform_dataset(x_train)
+    x_val_prep = transform_dataset(x_val)
     return x_train_prep, x_val_prep
 
 def DSpreparer(dataSet):
-    x_prep = data_preparer.transform(dataSet)
-    return x_prep
-
+    return transform_dataset(dataSet)
