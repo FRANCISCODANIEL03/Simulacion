d1d39871c5da2b35afafd96157965ee6d3b9d9c9
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-01-19 20:05:20 -0600

    read a new dataset

--- server2/model/load_divide_DS.py
+++ server2/model/load_divide_DS.py
@@ -1,31 +1,26 @@
 from load_kdd_dataset import load_kdd_dataset
-from train_val_test_split import train_val_test_split 
+from train_val_test_split import train_val_test_split
 
-df = load_kdd_dataset("Rice_MSC_Dataset.arff")
+df = load_kdd_dataset("Dry_Bean_Dataset.arff")
 
-#Division del DataSet en los diferentes subconjuntos
+# División del DataSet en los diferentes subconjuntos
 train_set, val_set, test_set = train_val_test_split(df)
 
+# Generar conjuntos `x` e `y` una vez
+x_df, y_df = train_set.drop("Class", axis=1), train_set["Class"].copy()
+x_train, y_train = train_set.drop("Class", axis=1), train_set["Class"].copy()
+x_val, y_val = val_set.drop("Class", axis=1), val_set["Class"].copy()
+x_test, y_test = test_set.drop("Class", axis=1), test_set["Class"].copy()
+
+# Funciones que simplemente retornan los valores globales
 def datasetGen():
-    # DataSet general 
-    x_df = df.drop("CLASS", axis = 1)
-    y_df = df["CLASS"].copy()
     return x_df, y_df
 
 def dsTrain():
-    # DataSet de entrenamiento 
-    x_train = train_set.drop("CLASS", axis = 1)
-    y_train = train_set["CLASS"].copy()
     return x_train, y_train
 
 def dsVal():
-    # DataSet de validacion
-    x_val = val_set.drop("CLASS", axis = 1)
-    y_val = val_set["CLASS"].copy()
     return x_val, y_val
 
 def dsTest():
-    # DataSet de test
-    x_test = test_set.drop("CLASS", axis = 1)
-    y_test = test_set["CLASS"].copy()
     return x_test, y_test
