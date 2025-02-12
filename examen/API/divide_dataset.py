9b9d56c6f5f6a2ab06715fcf778035e12e3e1d0e
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-11 23:08:19 -0600

    divide dataset

--- examen/API/divide_dataset.py
+++ examen/API/divide_dataset.py
@@ -4,3 +4,8 @@ from load_arff import load_kdd_dataset
 df = load_kdd_dataset('/home/pako0311/Escritorio/DATASET/Dry_Bean_Dataset.arff')
 
 train_set, val_set, test_set = train_val_test_split(df)
+
+def d_train_set():
+    x_train = train_set.drop('Class', axis=1)
+    y_train = train_set['Class'].copy()
+    return x_train, y_train
