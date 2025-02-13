594131380ab4a7c23ec77f39232156870d750588
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-12 23:10:22 -0600

    define variables of the API

--- examen/API/main.py
+++ examen/API/main.py
@@ -2,3 +2,6 @@ from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 from metrics import pred_metrics
 from polynomial_k import pred_reduced_f1s
+
+app = Flask(__name__)
+CORS(app)
