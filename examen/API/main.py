68bc5439de9865a293fa853e9c7df7d2f9e51c7f
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-12 23:10:43 -0600

    create endpoint

--- examen/API/main.py
+++ examen/API/main.py
@@ -5,3 +5,27 @@ from polynomial_k import pred_reduced_f1s
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/metrics2", methods = ["GET"])
+@cross_origin()
+def metrics():
+    F1_score, Pres, recall = pred_metrics()
+    #F1_score2, Pres2, recall2 = pred_metrics2()
+    return jsonify([
+        {
+            "name":"F1 score ",
+            "porcent":F1_score
+        },
+        {
+            "name":"Precision",
+            "porcent":Pres
+        },
+        {
+            "name":"Recall",
+            "porcent":recall
+        }
+      
+    ])
+
+if __name__ == '__main__':
+    app.run()
