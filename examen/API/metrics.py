4e8315716f938936987bb51d44d340c60b472090
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-12 23:11:19 -0600

    add imports

---
+++ examen/API/metrics.py
@@ -0,0 +1,5 @@
+from reduced_ds import red_ds
+from divide_dataset import d_val_set, d_train_set
+from prepare_ds import prepare_dataset
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score, precision_score, recall_score
