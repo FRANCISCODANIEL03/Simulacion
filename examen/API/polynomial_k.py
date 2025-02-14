a2e232ab4fe7e0883e5b47bd8e4d95255de2e835
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-13 23:14:38 -0600

    define variables

--- examen/API/polynomial_k.py
+++ examen/API/polynomial_k.py
@@ -5,3 +5,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score
+
+x_train, y_train = d_train_set()
+x_val, y_val = d_val_set()
+x_train_reduced, x_val_reduced = red_ds()
