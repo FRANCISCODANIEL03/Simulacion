a89162904bb46d94c2328c4ef088ed17970d4643
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-13 23:15:00 -0600

    define extra variables

--- examen/API/polynomial_k.py
+++ examen/API/polynomial_k.py
@@ -9,3 +9,7 @@ from sklearn.metrics import f1_score
 x_train, y_train = d_train_set()
 x_val, y_val = d_val_set()
 x_train_reduced, x_val_reduced = red_ds()
+
+# Para representar el limite de decision de tiene que pasar la variable objetivo a numerica 
+y_train_num = y_train.factorize()[0]
+y_val_num = y_val.factorize()[0]
