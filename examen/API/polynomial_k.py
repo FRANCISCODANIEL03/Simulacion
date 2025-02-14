c2004c004acab91b1e2efdfd8f9dbb58017c6962
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-13 23:14:17 -0600

    add imports

---
+++ examen/API/polynomial_k.py
@@ -0,0 +1,7 @@
+from divide_dataset import d_train_set, d_val_set
+from reduced_ds import red_ds
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
