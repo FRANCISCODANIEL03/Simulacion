ef267c8c04871d4a536c67bfb82efb38f83e400e
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-14 23:16:16 -0600

    prepare dataset

--- examen/API/prepare_ds.py
+++ examen/API/prepare_ds.py
@@ -5,3 +5,19 @@ import pandas as pd
 x_train, y_train = d_train_set()
 x_val, y_val = d_val_set()
 x_test, y_test = d_test_set()
+
+def prepare_dataset():
+    # Rellenar los atributos que contenan valores infinitos
+
+    imputer = SimpleImputer(strategy='median')
+
+    x_train_prep = imputer.fit_transform(x_train)
+    x_val_prep = imputer.fit_transform(x_val)
+    x_test_prep = imputer.fit_transform(x_test)
+
+    # Transformar el DataFrame de pandas
+    x_train_prep2 = pd.DataFrame(x_train_prep, columns = x_train.columns, index = y_train.index)
+    x_val_prep2 = pd.DataFrame(x_val_prep, columns = x_val.columns, index = y_val.index)
+    x_test_prep2 = pd.DataFrame(x_test_prep, columns = x_test.columns, index = y_test.index)
+
+    return x_train_prep2, x_val_prep2, x_test_prep2
