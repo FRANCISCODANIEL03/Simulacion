774d5de0e564ed4393a2c2f3d87371a1302dc85b
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-02-14 23:15:59 -0600

    define variables

--- examen/API/prepare_ds.py
+++ examen/API/prepare_ds.py
@@ -1,3 +1,7 @@
 from sklearn.impute import SimpleImputer
 from divide_dataset import d_train_set, d_val_set, d_test_set
 import pandas as pd
+
+x_train, y_train = d_train_set()
+x_val, y_val = d_val_set()
+x_test, y_test = d_test_set()
